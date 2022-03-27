b14cce335839b722bd3395818529f73eae02074b
Tino Tom <tinotom7@outlook.com>
2022-03-28 00:56:14 +0100

    added roaming state to enemy

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -4,13 +4,40 @@ using UnityEngine;
 
 public abstract class Enemy : MonoBehaviour, IHittable
 {
+
+    protected enum EnemyState { IDLE, ROAMING, FOLLOWING, RETURNING };
     [SerializeField] protected float m_health = 100f;
+    [SerializeField] protected bool m_boundFollow = false;
+    [SerializeField] protected float m_boundFollowRadius = 20f;
 
-    [SerializeField] protected Animator m_animator;
 
-    void Awake()
+    [SerializeField] protected EnemyState m_state = EnemyState.IDLE;
+    protected Animator m_animator;
+
+    protected virtual void Awake()
     {
         m_animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        switch (m_state)
+        {
+            case EnemyState.IDLE:
+                onIdle();
+                break;
+            case EnemyState.ROAMING:
+                onRoaming();
+                break;
+            case EnemyState.FOLLOWING:
+                onFollowing();
+                break;
+            case EnemyState.RETURNING:
+                onReturning();
+                break;
+            default:
+                break;
+        }
     }
 
     public virtual void onHit(float damageDealt)
@@ -18,6 +45,7 @@ public abstract class Enemy : MonoBehaviour, IHittable
         if (m_health > 0)
         {
             m_health -= damageDealt;
+            m_animator.SetTrigger("hit");
             if (m_health <= 0)
             {
                 m_health = 0f;
@@ -27,4 +55,13 @@ public abstract class Enemy : MonoBehaviour, IHittable
     }
 
     protected abstract void onDeath();
+    protected virtual void onIdle()
+    {
+
+    }
+    protected abstract void onRoaming();
+    protected abstract void onFollowing();
+    protected abstract void onReturning();
+
+
 }
--- Assets/Scripts/Bat.cs
+++ Assets/Scripts/Bat.cs
@@ -4,8 +4,24 @@ using UnityEngine;
 
 public class Bat : Enemy
 {
+    [SerializeField] private float m_roamSpeed;
+    private Vector2 m_targetPosition;
+    private Vector2 m_originPosition;
+    [SerializeField] private float m_roamingRadius;
+    [SerializeField] private LayerMask m_roamCollision;
 
+    protected override void Awake()
+    {
+        base.Awake();
+        m_targetPosition = transform.position;
+        m_originPosition = transform.position;
+        m_roamCollision = LayerMask.GetMask("Environment");
+    }
 
+    void Start()
+    {
+
+    }
     public override void onHit(float damageDealt)
     {
         base.onHit(damageDealt);
@@ -14,6 +30,7 @@ public class Bat : Enemy
 
     }
 
+
     protected override void onDeath()
     {
         m_animator.SetTrigger("dead");
@@ -21,5 +38,83 @@ public class Bat : Enemy
         GetComponent<BoxCollider2D>().enabled = true;
         GetComponent<CircleCollider2D>().enabled = false;
         Destroy(gameObject, m_animator.GetCurrentAnimatorStateInfo(0).length + 2f);
+    }
+
+    protected override void onFollowing()
+    {
+
+    }
+
+    protected override void onReturning()
+    {
+
+    }
+
+    protected override void onRoaming()
+    {
+        if (Vector2.Distance(m_targetPosition, transform.position) < 0.1f)
+        {
+            m_targetPosition = getNewTargetPosition();
+            if (m_targetPosition.x > transform.position.x)
+            {
+                flip(true);
+            }
+            else
+            {
+                flip(false);
+            }
+        }
+        transform.position = Vector2.MoveTowards(transform.position, m_targetPosition, m_roamSpeed * Time.deltaTime);
+    }
+
+    Vector2 getNewTargetPosition()
+    {
+        Vector2 pos = (Random.insideUnitCircle * m_roamingRadius) + m_originPosition;
+        RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, GetComponent<CircleCollider2D>().radius,
+            (pos - (Vector2)transform.position), Vector2.Distance(pos, transform.position), m_roamCollision);
+        while (hits.Length > 0)
+        {
+            pos = (Random.insideUnitCircle * m_roamingRadius) + m_originPosition;
+            hits = Physics2D.CircleCastAll(transform.position, GetComponent<CircleCollider2D>().radius,
+                (pos - (Vector2)transform.position), Vector2.Distance(pos, transform.position), m_roamCollision);
+        }
+        return pos;
+    }
+
+    void flip()
+    {
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+
+    void flip(bool faceRight)
+    {
+        Vector3 scale = transform.localScale;
+        scale.x = Mathf.Abs(scale.x);
+        if (!faceRight)
+        {
+            scale.x *= -1;
+        }
+        transform.localScale = scale;
+    }
+
+    void OnDrawGizmos()
+    {
+
+        if (Application.isPlaying)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(m_originPosition, m_roamingRadius);
+            Gizmos.color = Color.white;
+            Gizmos.DrawLine(transform.position, m_targetPosition);
+
+        }
+        else
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(transform.position, m_roamingRadius);
+
+        }
     }
 }
